8088085f10805ec33cbb8a77479511f1be457607
Erick Dos Ramos <erick2ramos@gmail.com>
2014-10-19 12:28:44 -0430

    Archivos que no se usan

--- src/getinput.m
+++
@@ -1,9 +0,0 @@
-function input = getinput(n)
-    global inp ;
-    input = zeros(5, 2) ;
-    for k=1:2
-        a = inp(n, k) ;
-        b = (k * 10) - 9 ;
-        input(b:(b + a - 1)) = ones(a, 2) ;
-    endfor
-endfunction
--- src/gettarget.m
+++
@@ -1,6 +0,0 @@
-function target = gettarget(n)
-    global inp ;
-    target = zeros(2, 1) ;
-    a = inp(n, 10) + 1 ;
-    target(a) = 1 ;
-endfunction
