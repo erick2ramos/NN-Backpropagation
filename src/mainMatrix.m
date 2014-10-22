b0a5d35edf84490aeb514355a84d54f1223a0eb6
Erick Dos Ramos <erick2ramos@gmail.com>
2014-10-21 22:40:12 -0430

    Mejorando el main, graficar el error cuadratico medio. Generador de puntos de misma cantidad tanto de area dentro como fuera del circulo.

--- src/mainMatrix.m
+++ src/mainMatrix.m
@@ -1,26 +1,16 @@
 maxEpochs = 3000;
-n_hidden = 2;
+n_hidden = 10;
 learning_rate = 0.01;
 
-inp = dlmread('../datos/datos_P1_2_SD2014_n500.txt');
+#inp = dlmread('../datos/datos_P1_2_SD2014_n500.txt');
+[ inp, tnp ] = makepoints(500);
 warning('off');
 
-# Normalizando datos
+# Preparando datos
 inputs = inp(:,1:2);
 targets = inp(:,3);
 
-inputs_mean = mean(inputs);
-inputs_std = std(inputs);
-inputs = (inputs(:,:) - inputs_mean(:,1)) / inputs_std(:,1);
-
-targets = targets';
-targets_mean = mean(targets);
-targets_std = std(targets);
-targets = (targets(:,:) - targets_mean(:,1)) / targets_std(:,1);
-targets = targets';
-
-[ validset, testset ] = makepoints(100);
-grid = linspace(0, 20, 100)';
+grid = linspace(0, 20, 40)';
 validset = zeros(length(grid)^2,3);
 for i = 1:length(grid)
     x_g_in = grid(i);
@@ -31,22 +21,16 @@ for i = 1:length(grid)
         else
             g_in = 1;
         endif
-        validset(i*length(grid)+j,:) = [ x_g_in y_g_in g_in ];
+        validset((i-1)*length(grid)+j,:) = [ x_g_in y_g_in g_in ];
     endfor
 endfor
 validin = validset(:,1:2);
 validout = validset(:,3);
-validin_mean = mean(validin);
-validin_std = std(validin);
-validin = (validin(:,:) - validin_mean(:,1)) / validin_std(:,1);
-validout_mean = mean(validout);
-validout_std = std(validout);
-validout = (validout(:,:) - validout_mean(:,1)) / validout_std(:,1);
 
 inputs = [ inputs ones(length(inputs),1) ];
 validin = [ validin ones(length(validin),1) ];
 
-# Vector del circulo
+# Vector del circulo para graficar
 angle = 0;
 increment = 2*pi/(100);
 for i = 1:101
@@ -54,8 +38,8 @@ for i = 1:101
     Y(i,:) = 7*sin(angle) + 10;
     angle += increment;
 end
-figure(1);
 
+# Iniciando los pesos en valores aleatorios [-0.5, 0.5]
 w_i_h = initweights(size(inputs,2),n_hidden);
 w_h_o = initweights(1,n_hidden);
 
@@ -64,46 +48,49 @@ for epoch = 1:maxEpochs
     #Entrenamiento
     for i = 1:size(inputs,1)
         random_input = round(rand * size(inputs,1) + 0.5);
-        t_input = inputs(random_input,:);
-        t_target = targets(random_input,1);
+        t_input = inputs(i,:);
+        t_target = targets(i,1);
 
         [ activations, errors ] = forwardpropagation(t_input, t_target, w_i_h, w_h_o);
         [ w_i_h, w_h_o ] = backpropagation(learning_rate, t_input, activations, errors, w_i_h, w_h_o);
     endfor
-    epoch_errors = 0;
-    #Validacion
+
+    #Validacion con la grilla de puntos
     predictions = w_h_o * activate(validin * w_i_h)';
     error = predictions' - validout;
     err(epoch) = (sum(error.^2))^0.5;
-    #plot(err, '*');
-    #drawnow;
-    if err(epoch) < 0.01;
-        fprintf("Sali porque aprendi\n");
+    if err(epoch) <  0.01;
+        fprintf("Sali porque aprendi\n")
         break
     endif
 endfor
 toc
 
+# Graficando el error cuadratico medio
+figure(1);
+plot(err, '*');
+
+epoch_errors = 0;
 figure(2);
 plot(X,Y,'k');
 hold on;
 for i = 1 : length(predictions);
     if predictions(i) < 0
-        predictions(i) = -1;
+        if validset(i, 3) == -1;
+            plot(validset(i,1), validset(i,2), 'g*');
+        else
+            epoch_errors += 1;
+            plot(validset(i,1), validset(i,2), 'r*');
+        endif
     else
-        predictions(i) = 1;
-    endif
-    if predictions(i) != validset(i, 3);
-        epoch_errors += 1;
-        plot(validset(i,1), validset(i,2), 'r*');
-        #drawnow;
-    elseif predictions(i) < 0
-        plot(validset(i,1), validset(i,2), 'g*');
-        #drawnow;
-    else
-        plot(validset(i,1), validset(i,2), 'b*');
-        #drawnow;
+        if validset(i,3) == 1;
+            plot(validset(i,1),validset(i,2), 'b+');
+        else
+            epoch_errors += 1;
+            plot(validset(i,1), validset(i,2), 'm*');
+        endif
     endif
 endfor
 hold off;
 
+fprintf("Errores: %d (%d)\nEfectividad: %f\n", epoch_errors, length(validset),1 - epoch_errors/length(validset))
--- src/makepoints.m
+++ src/makepoints.m
@@ -1,25 +1,36 @@
-function [Test, Train] = makepoints(nsize)
-    n = nsize;
-    Test = zeros(n,3);
-    Test(:,1) = 20.*rand(n,1);
-    Test(:,2) = 20.*rand(n,1);
-    for i=1:n
+function [Train, Test] = makepoints(nsize)
+    half_n = floor(nsize / 2);
+    half_n_up = nsize - half_n;
+    Train = zeros(nsize,3);
+    in_count = 1;
+    out_count = 1;
+    while half_n - in_count + 1 != 0
+        r_point = [ 20.*rand 20.*rand ];
+        if sqrt((r_point(1)-10)^2 + (r_point(2)-10)^2) < 7
+            Train(in_count, 1:2) = r_point;
+            Train(in_count, 3) = -1;
+            in_count += 1;
+        endif
+    endwhile
+    in_count -= 1;
+    while half_n_up - out_count + 1 != 0
+        r_point = [ 20.*rand 20.*rand ];
+        if sqrt((r_point(1)-10)^2 + (r_point(2)-10)^2) >= 7
+            Train(out_count + in_count, 1:2) = r_point;
+            Train(out_count + in_count, 3) = 1;
+            out_count += 1;
+        endif
+    endwhile
+    Train = Train(randperm(size(Train,1)),:);
+
+    Test = zeros(nsize,3);
+    Test(:,1) = 20.*rand(nsize,1);
+    Test(:,2) = 20.*rand(nsize,1);
+    for i=1:nsize
         if sqrt((Test(i,1)-10)^2 + (Test(i,2)-10)^2) < 7
             Test(i,3) = -1;
         else
             Test(i,3) = 1;
-        endif
-    endfor
-
-    n = nsize;
-    Train = zeros(n,3);
-    Train(:,1) = 20.*rand(n,1);
-    Train(:,2) = 20.*rand(n,1);
-    for i=1:n
-        if sqrt((Train(i,1)-10)^2 + (Train(i,2)-10)^2) < 7
-            Train(i,3) = -1;
-        else
-            Train(i,3) = 1;
         endif
     endfor
 endfunction
